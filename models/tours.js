62496aafc666886bbef2a4139916a40032228d9d
LesiaHoshovska <lesja26@gmail.com>
2021-11-21 15:11:01 +0200

    +

--- models/tours.js
+++ models/tours.js
@@ -6,6 +6,7 @@ const tourSchema = new Schema({
   title: {
     type: String,
     required: true,
+    trim: true,
     minlength: 3,
     maxlength: 20,
   },
@@ -27,21 +28,91 @@ const tourSchema = new Schema({
   country: {
     type: String,
     required: true,
+    trim: true,
     minlength: 2,
   },
-  startDate: {
-    type: Date,
-    required: true,
-  },
+  dates: new mongoose.Schema({
+    startDate: {
+      type: Array,
+      required: true,
+    },
+    endDate: {
+      type: Array,
+      required: true,
+    },
+  }),
+
   description: {
     type: String,
     required: true,
+    trim: true,
     minlength: 50,
   },
   imgSrc: {
     type: String,
     required: true,
   },
+  daysDescription: new mongoose.Schema({
+    dayTitle: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    daySubtitle: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10,
+      maxlength: 20,
+    },
+    dayDescription: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 50,
+    },
+  }),
+  activity: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  dificulties: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10,
+  },
+  priceIncluded: {
+    type: Array,
+    required: true,
+  },
+  priceExcluded: {
+    type: Array,
+    required: true,
+  },
+  gearList: new mongoose.Schema({
+    mainGear: {
+      type: Array,
+      required: true,
+    },
+    clothes: {
+      type: Array,
+      required: true,
+    },
+    boots: {
+      type: Array,
+      required: true,
+    },
+    anotherNesessaryGear: {
+      type: Array,
+      required: true,
+    },
+  }),
 });
 
 module.exports = mongoose.model("Tour", tourSchema);
